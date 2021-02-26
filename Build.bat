c05dc5c62d2461092a57e6f236d6167d012f4c6e
Bojan Antolović <bojan.antolovic@gmail.com>
2021-02-26 13:29:42 +0100

    Build scripts for .NET 5.

--- Build.bat
+++ Build.bat
@@ -2,15 +2,19 @@ SETLOCAL
 SET Version=5.0.0
 SET Prerelease=auto
 
-CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
+@SET Config=%1%
+@IF [%1] == [] SET Config=Debug
 
-REM Updating the build version of all projects.
+REM Updating the build version.
 PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
 
 WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
-NuGet restore -NonInteractive || GOTO Error0
-MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
-IF NOT EXIST Install md Install
+
+dotnet build Impersonation.sln --configuration %Config% -p:RhetosDeploy=false || GOTO Error0
+
+IF NOT EXIST Install MD Install
+DEL /F /S /Q Install\* || GOTO Error0
+
 NuGet pack .\src\Rhetos.Impersonation.nuspec -OutputDirectory Install || GOTO Error0
 
 REM Updating the build version back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
